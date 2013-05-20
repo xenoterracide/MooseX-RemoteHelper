eaf88609374d9817469fb428b30b063a13ed29b0
Caleb Cushing <xenoterracide@gmail.com>
2013-05-20 13:45:00 -0500

    use another type and a regex so that we can match case insensitive
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/MooseX/RemoteHelper/Types.pm
+++ lib/MooseX/RemoteHelper/Types.pm
@@ -7,7 +7,10 @@ use warnings;
 use MooseX::Types    -declare => [qw( Bool TrueFalse )];
 use MooseX::Types::Moose -all => { -prefix => 'Moose' };
 
-enum TrueFalse, [qw( true false True False )];
+subtype TrueFalse, as MooseStr,
+	where {
+		$_ =~ m/^(true|false)$/ixms;
+	};
 
 subtype Bool, as MooseBool;
 coerce  Bool, from TrueFalse,
